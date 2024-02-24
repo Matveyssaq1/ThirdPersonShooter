853b3407b329e031cce8bd3c5974d82db4ef9b7c
Matveyssaq1 <154542883+Matveyssaq1@users.noreply.github.com>
2024-02-24 17:04:32 +0300

    fireball
    added fireball and it's ovement added to player and tryed to spawn it

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+
+    void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+        
+        
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireball();  
+    }
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,27 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball firebalPrefab;
+    public Transform fireballSourceTransform;
+    void Start()
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+      FireballCasting();
+    }
+    private void FireballCasting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(firebalPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
