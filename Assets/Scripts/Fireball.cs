95f5068b6fb22654eb04349d7e192d36f064cb71
Matveyssaq1 <154542883+Matveyssaq1@users.noreply.github.com>
2024-02-24 21:51:08 +0300

    fireball update,damage
    finished fireball spawning + added damage for the mob

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -6,17 +6,16 @@ public class Fireball : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
-
+    public float damage = 10;
     void Start()
     {
         Invoke("DestroyFireball", lifeTime);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         MoveFixedUpdate();
-        
         
     }
     private void MoveFixedUpdate()
@@ -25,10 +24,21 @@ public class Fireball : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        DestroyFireball();  
+        DamageEnemy(collision);
+        DestroyFireball();
+
     }
     private void DestroyFireball()
     {
         Destroy(gameObject);
+    }
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+        
     }
 }
