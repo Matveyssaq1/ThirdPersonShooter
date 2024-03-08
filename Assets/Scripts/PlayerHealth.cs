5b38eae87f8facb226718feccad5dcdbede120bd
Matveyssaq1 <154542883+Matveyssaq1@users.noreply.github.com>
2024-03-08 16:51:33 +0300

    UI chabges
    added health bar end game and restart mechanics

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,6 +7,8 @@ public class PlayerHealth : MonoBehaviour
 
     public float health = 100;
     public RectTransform valueRectTransform;
+    public GameObject gameplayUI;
+    public GameObject gameOverScreen;
 
     private float _maxHealth;
 // Start is called before the first frame update
@@ -22,13 +24,22 @@ public class PlayerHealth : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            Destroy(gameObject);
+            gameOverScreen.SetActive(true);
+            gameplayUI.SetActive(false);
+            PlayerIsDead();
+
         }
         DrawHealthBar();
     }
     private void DrawHealthBar()
     {
         valueRectTransform.anchorMax = new Vector2(health / _maxHealth, 1) ;
+    }
+    private void PlayerIsDead()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<FireballCaster>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
     }
 
 }
