2ae23364ef87048e320ad7d4859e16c2e9433c91
Matveyssaq1 <154542883+Matveyssaq1@users.noreply.github.com>
2024-03-21 16:31:58 +0300

    ,

---
+++ Assets/Scripts/portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class portal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerHealth>() != null)
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(index + 1);
+        }
+    }
+}
